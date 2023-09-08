a9e2486e49feec6ea6bd0e1cf603bc6ae8e9a998
Rokachello <gabrovsek.rok@hotmail.com>
2023-09-09 00:43:05 +0200

    game logic added o or x win

--- script.js
+++ script.js
@@ -1,49 +1,66 @@
 
-// Gameboard
-const gameboard = (() => {
-    let board = [["x","o","o"],["o","x","x"],["x","o","x"]]
-    const chooseField = (i,j) => {
-        return board[i][j]
-    }
-    const updateField = (i, j, symbol) => {
-        board[i][j] = symbol;
-    }
-    return {
-      chooseField,
-      updateField
-    }
-  })()
+let clickedCelsP1 = []
+let clickedCelsP2 = []
+let endGame = false
+const symbolX = "x"
+const symbolO= "o"
+let currentSymbol ="x"
+let currentPlayer = clickedCelsP1
+const winnerText = document.querySelector(".win")
 
-  //Player
-  const player = (symbol) =>{
-    
-    const play = (i,j) => {
-        gameboard.updateField(i,j, symbol) 
+const winningCombinations = [[1,2,3],[4,5,6],[7,8,9],
+                             [1,4,7],[2,5,8],[3,6,9],
+                             [1,5,9],[3,5,7]]
+
+const nextSymbol = ()=>{
+    if(currentSymbol == symbolX){
+        currentSymbol = symbolO
+    } else {
+        currentSymbol = symbolX
     }
-    
-    return {
-        play,
+    return currentSymbol
+}
+const nextPlayer = () => {
+    if(currentPlayer==clickedCelsP1){
+        currentPlayer=clickedCelsP2
+    } else {
+        currentPlayer=clickedCelsP1
     }
-    };
+    return currentPlayer
+}
 
+const isWinner = (winningCombinations, playArray) => {
+    for (const winComb of winningCombinations) {
+      if (winComb.every((el) => playArray.includes(el))) {
+        return true; // Return true if a winning combination is found
+      }
+    }
+    return false; // Return false if no winning combination is found in the playArray
+  };
 
-
-  //Game control
-  const control =(() =>{
-    
-    const playerX = player("x")
-    const playerO = player("o")
-    let currentPlayer = playerX
-
-    const switchPlayer = () => {
-        if(currentPlayer === playerX){
-            currentPlayer = playerO
-        } else {
-            currentPlayer = playerX
+const cells = document.querySelectorAll(".cell")
+cells.forEach(cell => {
+    cell.addEventListener("click", () => {
+        let cellValue = cell.textContent
+        if(cellValue == "" && endGame == false){
+        currentPlayer.push(parseInt(cell.id)),
+        cell.textContent = currentSymbol;
+        let winner = isWinner(winningCombinations, currentPlayer);
+        if(winner){
+            if(clickedCelsP1 == currentPlayer){
+                console.log("P1 wins")
+                winnerText.textContent = "P1 -x- wins"
+            } 
+            if(clickedCelsP2 == currentPlayer){
+                console.log("P2 wins")
+                winnerText.textContent = "P2 -o- wins"
+            }
+            endGame = true
+        }
+        console.log(winner)
+        nextSymbol(),
+        nextPlayer(),
+       console.log("P1: " + clickedCelsP1 + "  P2: " + clickedCelsP2)
         }
     }
-    return {
-        switchPlayer
-    }
-  })();
-
+    )})
